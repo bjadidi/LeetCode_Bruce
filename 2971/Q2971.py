58774e9117e8277bc497d6eb3d4b8ae988d745e8
Behrooz <bjadidi@ryerson.ca>
2024-02-15 13:32:30 -0500

    Solved Q2971

--- 2971/Q2971.py
+++ 2971/Q2971.py
@@ -1,5 +1,14 @@
-nums = [5,5,5]
 
-poly_possible = True
+def solution(nums):
+    list_nums = nums
+    for item in list_nums:
+        max_num = max(list_nums)
+        list_nums.remove(max_num)
 
-if 
+        if sum(list_nums) > max_num and len(list_nums)>1:
+            return (sum(list_nums)+(max_num))
+        
+    return -1
+   
+
+nums = [1,2,3,4,5]
